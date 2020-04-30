e18ff20328450743c80cfaccc394dd66015f9f52
Alexander <o.nadtochyi@reply.it>
2020-04-30 21:21:00 +0200

    folders structure refactoring

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,29 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-import { Aux } from '../../../hoc/Aux'
+import { Aux } from '../../../hoc/Aux/Aux'
 import { Backdrop } from '../Backdrop/Backdrop'
 import classes from './Modal.module.css'
 
-export const Modal = ({show, hide, children}) => {
-    return (
-        <Aux>
-            <Backdrop show={show} onClose={hide}/>
-            <div 
-                className={classes.Modal} 
-                style={{
-                    transform: show ? 'translateY(0)' : 'translateY(-100vh)',
-                    opacity: show ? '1' : '0'
-                }}>
-                {children}
-            </div>
-        </Aux>
-    )
+export class Modal extends Component {
+
+    shouldComponentUpdate(nextProps, nextState){
+        return nextProps.show !== this.props.show
+    }
+
+    render() {
+        const {hide, show, children} = this.props
+        return (
+            <Aux>
+                <Backdrop show={show} onClose={hide}/>
+                <div 
+                    className={classes.Modal} 
+                    style={{
+                        transform: show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: show ? '1' : '0'
+                    }}>
+                    {children}
+                </div>
+            </Aux>
+        )
+    }
 }
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 
 import { MEAT, CHEESE, BACON, SALAD } from '../../components/Burger/burgerIngredientTypes'
-import { Aux } from '../../hoc/Aux'
+import { Aux } from '../../hoc/Aux/Aux'
 import { Burger } from '../../components/Burger/Burger'
 import { BuildControls } from '../../components/Burger/BuildControls/BuildControls'
 import { Modal } from '../../components/UI/Modal/Modal'
