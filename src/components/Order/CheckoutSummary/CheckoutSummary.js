d35abc32a8c035b3b89885791995fc4591c0b683
Alexander <o.nadtochyi@reply.it>
2020-05-02 14:13:48 +0200

    added navigation back and next in checkout page

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -4,15 +4,15 @@ import { Burger } from '../../Burger/Burger'
 import { Button } from '../../UI/Button/Button'
 import classes from './CheckoutSummary.module.css'
 
-export const CheckoutSummary = ({ingredients}) => {
+export const CheckoutSummary = ({ingredients, checkoutCancelled, checkoutContinued}) => {
     return (
         <div className={classes.CheckoutSummary}>
             <h1>We hope it tastes well!</h1>
             <div style={{width:'100%', margin: 'auto'}}>
                 <Burger ingredients={ingredients}/>
             </div>
-            <Button btnType='Danger'>CANCEL</Button>
-            <Button btnType='Success'>CONTINUE</Button>
+            <Button btnType='Danger' clicked={checkoutCancelled}>CANCEL</Button>
+            <Button btnType='Success' clicked={checkoutContinued}>CONTINUE</Button>
         </div>
     )
 }
