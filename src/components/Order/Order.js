39e174d4cef89c549e4d0a199ea741c129959001
Alexander <o.nadtochyi@reply.it>
2020-05-02 17:08:53 +0200

    setting orders data into orders page

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,11 +2,22 @@ import React from 'react'
 
 import classes from './Order.module.css'
 
-export const Order = () => {
+export const Order = ({ingredients, totalPrice}) => {
+    const ingrs = []
+    for(let ingName in ingredients){
+        ingrs.push({
+            ingrredient: ingName,
+            qty: ingredients[ingName]
+        })
+    }
     return (
         <div className={classes.Order}>
-            <p>Ingredients: Salad (1)</p>
-            <p>Price: <strong> USD 5.45</strong></p>
+             <p>Ingredients:</p>
+            {ingrs.map(item => (
+                item.qty > 0 ? <p key={item.ingrredient}>{item.ingrredient}: {item.qty}</p> : null
+            ))}
+            
+            <p>Price: <strong> USD {totalPrice.toFixed(2)}</strong></p>
         </div>
     )
 }
